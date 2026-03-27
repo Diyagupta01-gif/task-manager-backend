169f7145ba44f8f1004424397ef3e0d5480e6632
Diya Gupta <diyagupta@Diyas-MacBook-Air.local>
2026-03-27 13:58:50 +0530

    Backend: Task Manager API with auth

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cors = require('cors'); // ✅ ADD THIS
+
+// routes
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// ✅ ADD THIS (VERY IMPORTANT)
+app.use(cors());
+
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+// home route
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
+});
+
+// server
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
